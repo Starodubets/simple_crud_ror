b68ffcbb3c441aa24ceaabaf9e8905f5cd92f0e6
Starodubets <Starodubets@users.noreply.github.com>
2017-11-28 20:35:02 +0200

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+
+  def after_sign_in_path_for(resource)
+    user_path_url
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  devise_for :users
+  #get 'persons/profile'
+
+  #root 'persons#index'
+  root 'tasks#index'
+  get '/', to:'tasks#index', as:'user_path'
+  # resources :tasks do
+  #   get 'create', to: 'tasks#new'
+  #   post 'create', to: 'tasks#create'
+  # end
+  get 'tasks/new', to: 'tasks#new', as:'new_task'
+  get 'tasks/edit', to: 'tasks#edit', as:'edit_task'
+  post 'tasks/new', to: 'tasks#create', as:'create_task'
+  patch 'tasks/update/:id', to: 'tasks#update', as: 'update_task'
+  delete 'tasks/delete/:id', to: 'tasks#delete', as: 'delete_task'
+
+  get 'persons/profile', as: 'user_root'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
